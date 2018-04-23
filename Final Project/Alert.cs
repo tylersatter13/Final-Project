619ebb5cd57b0cbd246abab7181d9455a9ad3686
Tyler Satter <tylersatter@TYLERSATTER8469.SatterHomes.net>
2018-04-23 10:29:29 -0700

    Add project files.

---
+++ Final Project/Alert.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+namespace Final_Project
+{
+    class Alert
+    {
+        //Provides a  Yes or No question to the user an then returns a response to the calling method
+        public bool CreateChoiceAlert(int AlertType, string title, string description)
+        {
+            DialogResult result = MessageBox.Show(description, title, MessageBoxButtons.YesNo, DetermineMessageType(AlertType));
+
+             
+            if (result == DialogResult.Yes) //Determine if the user selected The postive or negative button
+            {
+                return true;
+            }
+            else
+            {
+                return false; // Represents no
+            }
+
+        }
+        //Provides and alert message of a selected type but 
+        public void CreateBasicAlert(int AlertType, string description, string title)
+        {
+            MessageBox.Show(title, description, MessageBoxButtons.OK, DetermineMessageType(AlertType) );
+        }
+        //Makes it easier to set Message Type and collect error by assigning them an a number based on their severity.
+        public MessageBoxIcon DetermineMessageType( int  alertType)
+        {
+            switch (alertType)
+            {
+                case 1:
+                    return MessageBoxIcon.Information; // will make the message box in to an information message box
+                case 2:
+                    return MessageBoxIcon.Question; // will make the message box in to an Question message box
+                case 3:
+                    return MessageBoxIcon.Error;  // will make the message box in to an Error message box 
+                case 4:
+                     return MessageBoxIcon.Warning; // will make the message box in to an Warnign message box
+                default:
+                    return MessageBoxIcon.None; // will make the message box in to a standard message box
+            }
+        }
+    }
+}
---
+++ Final Project/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Project
+{
+    class User
+    {
+        private String strUserName;
+        private int intRole;
+        private String strUserID;
+
+        public User(String strUserName, int intRole, String strUserID)
+        {
+            this.strUserName = strUserName;
+            this.intRole = intRole;
+            this.strUserID = strUserID;
+        }
+        public String getUserName()
+        {
+            return strUserName;
+        }
+        public int getIntRole()
+        {
+            return intRole;
+        }
+        public String getStrUserId()
+        {
+            return strUserID;
+        }
+
+    }
+}
